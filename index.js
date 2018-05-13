855ee1df0a2b59acb7ac061cfd6c5334f0217d28
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-13 21:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  
+  for (var i = 1, ){
+    
+  }
   if (katzDeliLine.length === 0){
     return `The line is currently empty.`;
   }
