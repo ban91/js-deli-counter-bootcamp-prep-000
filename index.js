5490665339d3ec4e09cae5e5225b9a5bc5c140b1
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-13 21:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ function takeANumber(katzDeliLine, name){
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
-function nowServing(katzDeliLine){
+function nowServing(katzDeliLine, name){
   if (katzDeliLine.length === 0){
     return `There is nobody waiting to be served!`
   }
   else {
-    return `Currently serving ${katzDeliLine.length}`
+    return `Currently serving ${name}`;
   }
 }
